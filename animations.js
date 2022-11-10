3966786bca8dd79167b849fec6a7b3756e2d2c57
DavidLander1995 <63164991+DavidLander1995@users.noreply.github.com>
2022-11-10 18:09:35 +0100

    NEW.

--- animations.js
+++ animations.js
@@ -1,29 +1,92 @@
-// Homepage Animations 
+gsap.registerPlugin(ScrollTrigger)
 
-// Section 2 
-ScrollReveal().reveal('.home1a-animate', { viewFactor: 0.5, duration: 1000, reset: false});
 
-// Section 4
-ScrollReveal().reveal('.home1b-animate', { viewFactor: 0.5, duration: 1000, reset: false});
+gsap.from(".navbar", {
+  duration: 1,
+  y: "-100%",
+  ease: 'bounce',
+});
 
-// Section 6 
-ScrollReveal().reveal('.home2a-animate', { viewFactor: 0.5, duration: 1000, reset: false});
+// Logo
+gsap.to(".intro-logo", {
+  duration: 0,  
+  y: -100,
+  scale: 0,
+  opacity: 0,
+});
 
-// Section 8
-ScrollReveal().reveal('.home2b-animate', { viewFactor: 0.5, duration: 1000, reset: false});
+gsap.to(".intro-logo", {
+  duration: 2,  
+  y: 0, 
+  scale: 1,
+  opacity: 1,
+  ease: "power2",
+  scrollTrigger : { 
+    trigger: ".index-start", 
+    markers: true,
+    start: "top 90%",
+    end: "center 30%",
+  }, 
+});
 
-// Section 10 
-ScrollReveal().reveal('.home3a-animate', { viewFactor: 0.5, duration: 1000, reset: false});
 
-// Section 12 
-ScrollReveal().reveal('.home3b-animate', {viewFactor: 0.5, duration: 1000, reset: false});
 
-// Section 12 
-ScrollReveal().reveal('.home3c-animate', {viewFactor: 0.5, duration: 1000, reset: false});
+// Banner
+gsap.to([".banneranimate1", ".banneranimate3"], {
+  duration: 0,  
+  y: 100, 
+  opacity: 0,
+});
 
-// Section 14
-ScrollReveal().reveal('.home4a-animate', { viewFactor: 0.5, duration: 1000, reset: false});
+gsap.to(".banneranimate2", {
+  duration: 0,  
+  y: -100, 
+  opacity: 0,
+});
 
-// Section 14
-ScrollReveal().reveal('.home4b-animate', { viewFactor: 0.5, duration: 1000, reset: false});
+gsap.to([".banneranimate1", ".banneranimate3"], {
+  duration: 1,  
+  y: 0, 
+  opacity: 1,
+  stagger: 0.5,
+  scrollTrigger : { 
+    trigger: "#banner-selection", 
+    markers: false,
+    start: "top 40%",
+    end: "center 30%",
+  }, 
+});
 
+gsap.to(".banneranimate2", {
+  duration: 1,  
+  y: 0, 
+  opacity: 1,
+  scrollTrigger : { 
+    trigger: "#banner-selection", 
+    markers: false,
+    start: "top 40%",
+    end: "center 30%",
+  }, 
+});
+// Banner
+
+// Experiences 
+gsap.to(".experienceanimate1", {
+  duration: 0,  
+  x: 500, 
+  opacity: 0,
+});
+
+gsap.to(".experienceanimate1", {
+  duration: 2,  
+  x: 0, 
+  opacity: 1,
+  ease: "slow",
+  scrollTrigger : { 
+    trigger: ".experience-intro", 
+    markers: false,
+    start: "top 30%",
+    end: "center 30%",
+  }, 
+});
+// Experiences

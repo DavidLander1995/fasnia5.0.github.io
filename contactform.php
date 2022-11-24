fac7dc733ce4406ef8eeaeaf97062469ac37e78c
DavidLander1995 <63164991+DavidLander1995@users.noreply.github.com>
2022-11-24 18:01:00 +0100

    NEW.

--- contactform.php
+++ contactform.php
@@ -1,43 +1,18 @@
-<!-- THIS IS USING GOOGLES reCaptcha V2 -->
+<?php
 
-<?php 
-    if(isset($_POST['ContactButton'])) {
-        $url = "https://www.google.com/recaptcha/api/siteverify";
-        $privateKey = "PUT YOUR PRIVATE KEY HERE";
-        $response = file_get_contents($url."?secret=".$privateKey."&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR']);
-        $data = json_decode($response);
-        if (isset($data->success) AND $data->success==true) {
-            $error = "";
-            $successMsg = "";
-            if ($_POST) {
-                if ($_POST['email'] && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
-                    $error .= "The email is invalid!<br>";
-                }
-                if (!$_POST['email']) {
-                    $error .= "An email address is required!<br>";
-                }
-                if (!$_POST['subject']) {
-                    $error .= "A subject is required!<br>";
-                }
-                if (!$_POST['body']) {
-                    $error .= "Content in the body is required!<br>";
-                }
-                if ($error != "") {
-                    $error = '<div class="alert alert-danger" role="alert"><strong>There is an error with your form!</strong><br>' . $error . '</div>';
-                } else {
-                    $emailTo = 'CONTACT FORM DELIVERY EMAIL';
-                    $subject = $_POST['subject'];
-                    $body = $_POST['body'];
-                    $headers = "From: ".$_POST['email'];
-                    if (mail($emailTo, $subject, $body, $headers)) {
-                        $successMsg = '<div class="alert alert-success" role="alert">The message has successfully been sent. We will contact you ASAP!</div>';
-                    } else {
-                        $error = '<div class="alert alert-danger" role="alert">There was a problem sending your message, please try again later!</div>';
-                    }
-                }
-            }
-        } else {
-            $captchaFail = '<div class="alert alert-danger" role="alert"><strong>There is an error with your form!</strong><br>reCaptcha Verification Failed, Please Try Again.</div>';
-        }
-    }
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "lander.david@rocketmail.com";
+    $headers = "From: ".$mailFrom;
+    $text = "You have recieved an Email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $text, $headers);
+    header("Location: contacto.php?mailsend");
+
+}
+
 ?>
